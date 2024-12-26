ff9453fb1974054b8c2919ab3cc99dbb49eb40c0
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-12-26 12:37:18 +0700

    test

---
+++ src/app/Profile/page.tsx
@@ -0,0 +1,24 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <ul>
+        <li>
+          <a href="">Item 1</a>
+        </li>
+        <li>
+          <a href="">Item 2</a>
+        </li>
+        <li>
+          <a href="">Item 3</a>
+        </li>
+        <li>
+          <a href="">Item 4</a>
+        </li>
+        <li>
+          <a href="">Item 5</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
