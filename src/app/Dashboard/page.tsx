730394e350ab7cd4d96284e4c77ba921c6e02532
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-12-26 11:28:17 +0700

    create UI Dashboard

---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,24 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Hihi</h1>
+      <ul>
+        <li>
+          <a href="">Item 1</a>
+        </li>
+        <li>
+          <a href="">Item 2</a>
+        </li>
+        <li>
+          <a href="">Item 3</a>
+        </li>
+        <li>
+          <a href="">Item 4</a>
+        </li>
+        <li>
+          <a href="">Item 5</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
