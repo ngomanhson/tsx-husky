2d3da89f6ec495c1f1abb5e25adec12865fcc459
ngomanhson <ngomanhson2004txpt@gmail.com>
2024-12-26 15:53:43 +0700

    test

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -4,19 +4,19 @@ export default function Dashboard() {
       <h1>Hihi</h1>
       <ul>
         <li>
-          <a href="">Item 1</a>
+          <a href="/Item/1">Item 1</a>
         </li>
         <li>
-          <a href="">Item 2</a>
+          <a href="/Item/2">Item 2</a>
         </li>
         <li>
-          <a href="">Item 3</a>
+          <a href="/Item/3">Item 3</a>
         </li>
         <li>
-          <a href="">Item 4</a>
+          <a href="/Item/4">Item 4</a>
         </li>
         <li>
-          <a href="">Item 5</a>
+          <a href="/Item/5">Item 5</a>
         </li>
       </ul>
     </div>
